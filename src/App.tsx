2e6fd11eb2868ec966004bb50a1cd6f861d739c5
Arkady Polhovsky <arkims@inbox.ru>
2021-05-10 17:48:04 +0300

    lesson 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from './TodoList';
+
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+  const tasks: Array<TaskType> = [
+    {id: 1, title: 'HTML', isDone: true},
+    {id: 2, title: 'CSS', isDone: true},
+    {id: 3, title: 'React', isDone: false},
+    {id: 4, title: 'Redux', isDone: false},
+  ]
+
+  return (
+      <div className="App">
+        <TodoList title={'What to learn'} tasks={tasks}/>
+      </div>
+  );
 }
 
 export default App;
